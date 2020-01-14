5fdb8a5aea29c4423a83afdc6643fdd01eb5633d
Bill DuGe <billduge11@gmail.com>
2020-01-13 22:20:52 -0500

    Updates

---
+++ precache-manifest.eb93fabf8be0001e3051f913c0fa074a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62c06514e6432c1d48f11157d65e296f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ab6349253efe4b735123",
+    "url": "/static/css/main.fd442743.chunk.css"
+  },
+  {
+    "revision": "c672f962201f937cf181",
+    "url": "/static/js/2.d86169c0.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.d86169c0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ab6349253efe4b735123",
+    "url": "/static/js/main.6e9ce3b0.chunk.js"
+  },
+  {
+    "revision": "ff9369ce5e5734d35430",
+    "url": "/static/js/runtime-main.e1b86037.js"
+  }
+]);
